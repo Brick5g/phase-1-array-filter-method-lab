d5e8f7245414b7e0cce9750343122c9a0e55a114
Julian Francis <jgfrancis3@gmail.com>
2026-05-10 09:51:02 -0400

    Array Filter lab completed using ES6

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solution here
+const findMatching = (drivers, string) => 
+  drivers.filter(driver => driver.toLowerCase() === string.toLowerCase());
+
+const fuzzyMatch = (drivers, string) => 
+  drivers.filter(driver => driver.toLowerCase().startsWith(string.toLowerCase()));
+
+const matchName = (drivers, string) => 
+  drivers.filter(driver => driver.name.toLowerCase() === string.toLowerCase());
